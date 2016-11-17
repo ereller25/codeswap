e0fb8a5fcc47418060a28412c529daae720c3ada
Evan Reller <ereller@Evans-MacBook-Pro.local>
2016-11-17 09:37:42 -0500

    initial commit

---
+++ mongoUtil.js
@@ -0,0 +1,23 @@
+var MongoClient = require( 'mongodb' ).MongoClient;
+
+var _db;
+
+module.exports = {
+
+  connectToServer: function( callback ) {
+  	// connects to our database called shoppingLists
+    MongoClient.connect("mongodb://admin:admin@ds033066.mlab.com:33066/js1", function( err, db ) {
+      // assigns return database to var _db
+      //mongodb://admin:admin@ds033066.mlab.com:33066/js1
+      //mongodb://localhost:27017/shoppingLists
+      _db = db;
+      return callback( err );
+    } );
+  },
+
+  // function that we will use to access our database in other modules without having to connet again
+  getDb: function() {
+    return _db;
+  }
+  
+};
